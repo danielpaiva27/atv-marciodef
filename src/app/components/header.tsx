10e35e097bf868cf07c3d54f64d3252f606edf94
Daniel Luna <daniel.00000853754@unicap.br>
2025-04-25 15:30:37 -0300

    responsividade-again

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header-container">
       <div className="header-content">
-
-        <nav className="header-nav">
-          <Link href="../atividades">Início</Link>
-          <Link href="../sobre">Sobre Mim</Link>
-          <Link href="../conversores/main">Conversores</Link>
-          <Link href="#" >Em breve</Link>
+        <div className="header-logo">Trabalho de Atividades</div>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav className={`header-nav ${menuOpen ? "active" : ""}`}>
+          <Link href="/">Início</Link>
+          <Link href="/sobre">Sobre Mim</Link>
+          <Link href="/atividades">Atividades</Link>
+          <Link href="/conversores/main">Conversores</Link>
         </nav>
       </div>
     </header>
